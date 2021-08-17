c5e7238e6d2ad1aa716356bf46d9e9713fa26dc9
AnsloneKritisch <digveshoptimus@gmail.com>
2021-08-17 22:42:36 +0530

    five

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-import flask import flask , render_template ,requests
+from flask import Flask , render_template ,request
 from flask_mail import Mail , Message
+
+
 app = Flask(__name__)
+
 mail = Mail(app)
-@app.route(/)
+
+@app.route('/')
 def index():
     return render_template("contact.html")
+
+@app.route('/send_message' , methods = ['GET' , 'POST'] )
+def send_message():
+    if request.method == "POST" :
+        
+
 
 if __name__ == "__main__" :
     app.run(debug=True)
